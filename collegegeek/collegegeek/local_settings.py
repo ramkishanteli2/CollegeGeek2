9b247e902e127f93d2fa660772d296307c0551e0
Ram Kishan Teli <ramkishanteli2@gmail.com>
2022-02-19 00:39:11 +0530

    add requirements.txt

---
+++ collegegeek/collegegeek/local_settings.py
@@ -0,0 +1,10 @@
+import os
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
+    }
+}
