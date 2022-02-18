ade902e22c3c0ea3e5c2ffdf616a58fa3e6af675
= <=>
2022-02-19 01:30:31 +0530

    settings.py updated

--- collegegeek/collegegeek/urls.py
+++ collegegeek/collegegeek/urls.py
@@ -26,6 +26,4 @@ urlpatterns = [
     path('coding/', include('coding.urls')),
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
 ]
