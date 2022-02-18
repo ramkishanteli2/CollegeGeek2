945ccf477c18f163a1637fa601edb44911936911
= <=>
2022-02-19 01:23:50 +0530

    change done

--- collegegeek/collegegeek/urls.py
+++ collegegeek/collegegeek/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls import url
-import settings
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
